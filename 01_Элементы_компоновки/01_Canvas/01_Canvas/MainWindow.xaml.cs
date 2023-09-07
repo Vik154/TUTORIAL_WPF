bc05a9176c67f5edf317f15dec6cb7c71d47c9c6
Vik154 <Smorodin_1209@mail.ru>
2023-09-07 23:21:08 +0700

    Работа с Canvas

--- 01_Элементы_компоновки/01_Canvas/01_Canvas/MainWindow.xaml.cs
+++ 01_Элементы_компоновки/01_Canvas/01_Canvas/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ public partial class MainWindow : Window {
         InitializeComponent();
         Pages.Add(new _01_CanvasBase());
         Pages.Add(new _02_CanvasFromCode());
-        
+        Pages.Add(new _03_CanvasEvents());
+
     }
 
     /// <summary> Отображает страницу 01_CanvasBase </summary>
@@ -20,7 +21,13 @@ public partial class MainWindow : Window {
         _frame.Content = Pages[0];
     }
 
+    /// <summary> Отображает страницу 02_CanvasFromCode </summary>
     private void _02_Canvas_Click(object sender, RoutedEventArgs e) {
         _frame.Content = Pages[1];
+    }
+
+    /// <summary> Отображает страницу 03_CanvasEvents </summary>
+    private void _03_Canvas_Click(object sender, RoutedEventArgs e) {
+        _frame.Content = Pages[2];
     }
 }
