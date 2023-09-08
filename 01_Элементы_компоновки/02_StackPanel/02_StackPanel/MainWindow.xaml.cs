4b560c74127d57f12469bd42063be8271f701e60
Vik154 <Smorodin_1209@mail.ru>
2023-09-08 14:10:20 +0700

    Про

--- 01_Элементы_компоновки/02_StackPanel/02_StackPanel/MainWindow.xaml.cs
+++ 01_Элементы_компоновки/02_StackPanel/02_StackPanel/MainWindow.xaml.cs
@@ -1,25 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _02_StackPanel {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _02_StackPanel;
+
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    /* Программное создание StackPanel*/
+    private void MakeStackPanel() {
+        StackPanel stackPanel = new StackPanel {            // Создание объекта стек-панель
+            VerticalAlignment = VerticalAlignment.Top,      // Задает вертикальное выравнивание
+            HorizontalAlignment = HorizontalAlignment.Left, // Задает горизонтальное выравнивание
+            Orientation = Orientation.Vertical,             // Размещение элементов внутри стек панели
+            Width = 200,                                    // Ширина
+            Height = 200,                                   // Высота
+            Background = Brushes.AliceBlue                  // Цвет фона
+        };
+
+        for (int i = 0; i < 5; ++i) {                       // Добавление 5 кнопок в стек панель
+            stackPanel.Children.Add(new Button {            // Создание кнопки
+                Content = $"Кнопка {i + 1}",                // Надпись на кнопке
+                Height = 30,                                // Высота
+                Width = 150,                                // Ширина
+                FontWeight = FontWeights.Bold,              // Жирный шрифт
+                Margin = new Thickness(5, 5, 0, 0),         // Внешние отступы left,top,r,b
+
+                // Рандомная генерация цвета кнопки
+                Background = new SolidColorBrush(Color.FromRgb(
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255)
+                    ))
+            });
+        };
+
+        this.Content = stackPanel;
     }
 }
