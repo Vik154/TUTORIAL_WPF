935559dc61abbacea496b241242da7c0236527db
Vik154 <Smorodin_1209@mail.ru>
2023-09-08 19:20:30 +0700

    Программная генерация элементов

--- 01_Элементы_компоновки/04_DockPanel/MainWindow.xaml.cs
+++ 01_Элементы_компоновки/04_DockPanel/MainWindow.xaml.cs
@@ -1,25 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _04_DockPanel {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _04_DockPanel;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        MakeDockPanel();
     }
-}
+
+    /* Программное создание DockPanel*/
+    private void MakeDockPanel() {
+        DockPanel dockPanel = new DockPanel {               // Создание контейнера
+            VerticalAlignment = VerticalAlignment.Top,      // Задает вертикальное выравнивание
+            HorizontalAlignment = HorizontalAlignment.Left, // Задает горизонтальное выравнивание
+            Background = Brushes.AliceBlue                  // Цвет фона
+        };
+
+        for (int i = 0; i < 20; ++i) {                      // Добавление нопок
+            Button btn = new Button {                       // Создание кнопки
+                Content = $"Кнопка {i + 1}",                // Надпись на кнопке
+                FontWeight = FontWeights.Bold,              // Жирный шрифт
+                Margin = new Thickness(5, 5, 0, 0),         // Внешние отступы left,top,r,b
+
+                // Рандомная генерация цвета кнопки
+                Background = new SolidColorBrush(Color.FromRgb(
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255)
+                    ))
+            };
+
+            dockPanel.Children.Add(btn);            // Добавление в родительский контейнер
+            DockPanel.SetDock(btn, (Dock)(i % 4));  // Выбор положения (Doc.Top/Right/Left..)
+        
+        }; // for
+
+        this.Content = dockPanel;
+    
+    } // method MakeDockPanel()
+
+} // class MainWindow
