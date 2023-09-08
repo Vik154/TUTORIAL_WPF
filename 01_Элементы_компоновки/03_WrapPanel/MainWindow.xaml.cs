a862c8b2e5c77d7b1e126d969967cf5721e80824
Vik154 <Smorodin_1209@mail.ru>
2023-09-08 17:34:07 +0700

    Генерация WrapPanel из кода

--- 01_Элементы_компоновки/03_WrapPanel/MainWindow.xaml.cs
+++ 01_Элементы_компоновки/03_WrapPanel/MainWindow.xaml.cs
@@ -1,25 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _03_WrapPanel {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _03_WrapPanel;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        MakeWrapPanel();
+    }
+
+    /* Программное создание WrapPanel*/
+    private void MakeWrapPanel() {
+        WrapPanel wrapPanel = new WrapPanel {               // Создание объекта WrapPanel
+            VerticalAlignment = VerticalAlignment.Top,      // Задает вертикальное выравнивание
+            HorizontalAlignment = HorizontalAlignment.Left, // Задает горизонтальное выравнивание
+            Orientation = Orientation.Horizontal,           // Размещение элементов внутри контейнера
+            Background = Brushes.AliceBlue                  // Цвет фона
+        };
+
+        for (int i = 0; i < 20; ++i) {                      // Добавление нопок
+            wrapPanel.Children.Add(new Button {             // Создание кнопки
+                Content = $"Кнопка {i + 1}",                // Надпись на кнопке
+                Height = new Random().Next(20, 100),        // Высота
+                Width = new Random().Next(50, 150),         // Ширина
+                FontWeight = FontWeights.Bold,              // Жирный шрифт
+                Margin = new Thickness(5, 5, 0, 0),         // Внешние отступы left,top,r,b
+
+                // Рандомная генерация цвета кнопки
+                Background = new SolidColorBrush(Color.FromRgb(
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255)
+                    ))
+            });
+        };
+
+        this.Content = wrapPanel;
     }
 }
