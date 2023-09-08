886ee5fde363d66c0bb6ae38c3643da52ee0988a
Vik154 <Smorodin_1209@mail.ru>
2023-09-08 23:09:17 +0700

    Работа с Grid из кода

--- 01_Элементы_компоновки/05_Grid/MainWindow.xaml.cs
+++ 01_Элементы_компоновки/05_Grid/MainWindow.xaml.cs
@@ -13,13 +13,40 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _05_Grid {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _05_Grid;
+
+public partial class MainWindow : Window {
+    
+    public MainWindow() {
+        InitializeComponent();
+        MakeGrid();
+    }
+
+    private void MakeGrid() {
+        Grid grid = new Grid();
+        grid.ShowGridLines = true;
+        grid.ColumnDefinitions.Add(new ColumnDefinition());
+        grid.ColumnDefinitions.Add(new ColumnDefinition());
+        grid.RowDefinitions.Add(new RowDefinition());
+        grid.RowDefinitions.Add(new RowDefinition());
+
+        Rectangle rectangle = new Rectangle { Fill = Brushes.Aqua, Width = 100, Height = 50 };
+        Ellipse ellipse = new Ellipse { Fill = Brushes.Aqua, Width = 100, Height = 50 };
+        Button button1 = new Button { Content = "Кнопка", Background = Brushes.Beige };
+        Button button2 = new Button { Content = "Кнопка", Background = Brushes.Azure };
+
+        grid.Children.Add(rectangle); 
+        grid.Children.Add(ellipse);
+        grid.Children.Add(button1);
+        grid.Children.Add(button2);
+
+        Grid.SetColumn(rectangle, 1);
+        Grid.SetRow(rectangle, 0);
+        Grid.SetRow(ellipse, 1);
+        Grid.SetColumn(ellipse, 0);
+        Grid.SetRow(button1, 1);
+        Grid.SetColumn(button1, 1);
+
+        this.Content = grid;
     }
 }
