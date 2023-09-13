7e198ffec7d32fe93566251b910720740eae97d0
Vik154 <Smorodin_1209@mail.ru>
2023-09-13 14:25:22 +0700

    Программное создание checkBox

--- 02_Элементы_управления/02_CheckBox_and_RadioButton/MainWindow.xaml.cs
+++ 02_Элементы_управления/02_CheckBox_and_RadioButton/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace _02_CheckBox_and_RadioButton;
 
@@ -7,6 +9,27 @@ public partial class MainWindow : Window {
     
     public MainWindow() {
         InitializeComponent();
+        MakeCheckBox();
+    }
+
+    // Программное добавление флажков
+    private void MakeCheckBox() {
+        StackPanel wrapPanel = new StackPanel { Background = Brushes.AliceBlue };
+
+        for (int i = 0; i < 10; i++) {
+            CheckBox checkBox = new CheckBox {
+                Content = $"Check {i + 1}",
+                Margin = new Thickness(5),
+                Background = new SolidColorBrush(Color.FromRgb(
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255),
+                    (byte)new Random().Next(0, 255)
+                    )),
+                IsChecked = i % 2 == 0? true : false,
+            };
+            wrapPanel.Children.Add(checkBox);
+        }
+        this.Content = wrapPanel;
     }
 
     private void bigCheckBox_Checked(object sender, RoutedEventArgs e) {
