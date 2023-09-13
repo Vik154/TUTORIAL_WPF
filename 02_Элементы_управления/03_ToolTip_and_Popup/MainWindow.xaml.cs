18348794d944e3573d0f72f8a4451dca03a7ab46
Vik154 <Smorodin_1209@mail.ru>
2023-09-13 22:03:50 +0700

    Создание ToolTip из кода

--- 02_Элементы_управления/03_ToolTip_and_Popup/MainWindow.xaml.cs
+++ 02_Элементы_управления/03_ToolTip_and_Popup/MainWindow.xaml.cs
@@ -1,25 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _03_ToolTip_and_Popup {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _03_ToolTip_and_Popup;
+
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        InitializeComponent();
+        MakeToolTip();
+    }
+
+    private void MakeToolTip() {
+        Button button = new Button {
+            Content = "BIG BUTTON",
+            Background = Brushes.Tan
+        };
+
+        ToolTip toolTip = new ToolTip();
+        StackPanel stackPanel = new StackPanel();
+
+        stackPanel.Children.Add(new TextBlock { Text = "Подсказка1" });
+        stackPanel.Children.Add(new TextBlock { Text = "Подсказка2" });
+        stackPanel.Children.Add(new Button { Content = "Кнопка" });
+
+        toolTip.Content = stackPanel;
+        button.ToolTip = toolTip;
+
+        this.Content = button;
     }
 }
