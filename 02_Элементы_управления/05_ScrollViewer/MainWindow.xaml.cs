bb8ac0017cc24614b4fabe9bfe8cb28f27f203e1
Vik154 <Smorodin_1209@mail.ru>
2023-09-14 17:59:12 +0700

    ScrollViewer пример работы

--- 02_Элементы_управления/05_ScrollViewer/MainWindow.xaml.cs
+++ 02_Элементы_управления/05_ScrollViewer/MainWindow.xaml.cs
@@ -1,41 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace _05_ScrollViewer {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _05_ScrollViewer; 
 
-        private void UpClick(object sender, RoutedEventArgs e) {
-            _scroll.LineUp();
-        }
+public partial class MainWindow : Window {
 
-        private void DownClick(object sender, RoutedEventArgs e) {
-            _scroll.LineDown();
-        }
+    public MainWindow() {
+        InitializeComponent();
+    }
 
-        private void LeftClick(object sender, RoutedEventArgs e) {
-            _scroll.LineLeft();
-        }
+    private void UpClick(object sender, RoutedEventArgs e) {
+        _scroll.LineUp();
+    }
 
-        private void RightClick(object sender, RoutedEventArgs e) {
-            _scroll.LineRight();
-        }
+    private void DownClick(object sender, RoutedEventArgs e) {
+        _scroll.LineDown();
+    }
+
+    private void LeftClick(object sender, RoutedEventArgs e) {
+        _scroll.LineLeft();
+    }
+
+    private void RightClick(object sender, RoutedEventArgs e) {
+        _scroll.LineRight();
     }
 }
