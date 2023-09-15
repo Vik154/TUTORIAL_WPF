d9b337247fa4721db8cad246235c2787a9087a92
Vik154 <Smorodin_1209@mail.ru>
2023-09-15 13:19:51 +0700

    Программное создание ComboBox

--- 02_Элементы_управления/08_ComboBox/MainWindow.xaml.cs
+++ 02_Элементы_управления/08_ComboBox/MainWindow.xaml.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _08_ComboBox {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _08_ComboBox;
+
+public record class Person(string Name, string Company, int ID);
+
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        InitializeComponent();
+        MakeComboBox();
+    }
+
+    private void MakeComboBox() {
+        ComboBox comboBox = new ComboBox();
+        StackPanel stackPanel = new StackPanel();
+
+        comboBox.Items.Add(new Person("Tom", "Microsoft", 1));
+        comboBox.Items.Add(new Person("Tim", "Yandex", 2));
+        comboBox.Items.Add(new Person("Tor", "Google", 3));
+
+        comboBox.IsEditable = true;
+        comboBox.Text = "Сотрудники";
+
+        stackPanel.Children.Add(comboBox);
+
+        this.Content = stackPanel;
     }
 }
