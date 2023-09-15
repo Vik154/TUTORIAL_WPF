ac533571632ef57913c5aaf9de39d7b0690943ff
Vik154 <Smorodin_1209@mail.ru>
2023-09-15 14:32:46 +0700

    Пример DataGrid

--- 02_Элементы_управления/10_DataGrid/MainWindow.xaml.cs
+++ 02_Элементы_управления/10_DataGrid/MainWindow.xaml.cs
@@ -16,7 +16,14 @@ using System.Windows.Shapes;
 namespace _10_DataGrid; 
 
 public partial class MainWindow : Window {
+
     public MainWindow() {
         InitializeComponent();
     }
+}
+
+public class EmployeeInfoDataSource {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Number { get; set; }
 }
