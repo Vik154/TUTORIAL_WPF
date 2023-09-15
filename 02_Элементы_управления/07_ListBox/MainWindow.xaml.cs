51de1f0f011b3008b0df8c0dcae2b330ec6a6855
Vik154 <Smorodin_1209@mail.ru>
2023-09-15 12:12:33 +0700

    Программное создание ListBox

--- 02_Элементы_управления/07_ListBox/MainWindow.xaml.cs
+++ 02_Элементы_управления/07_ListBox/MainWindow.xaml.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _07_ListBox {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _07_ListBox;
+
+public record class Student(string Name, int Age, int ID, string Group);
+
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        InitializeComponent();
+        MakeListBox();
+    }
+
+    private void MakeListBox() {
+        ListBox listBox = new ListBox();
+
+        listBox.Items.Add(new Student("Tom", 22, 1, "21-A"));
+        listBox.Items.Add(new Student("Tim", 23, 2, "21-A"));
+        listBox.Items.Add(new Student("Ben", 21, 3, "13-A"));
+        listBox.Items.Add(new Student("Bob", 25, 4, "13-A"));
+        listBox.Items.Add(new Student("Sam", 24, 5, "13-A"));
+
+        listBox.Background = Brushes.AliceBlue;
+        listBox.FontWeight = FontWeights.Bold;
+        listBox.FontSize = 16;
+
+        this.Content = listBox;
     }
 }
