7e49abc8d18eb1a25d7d6cdd85c989c09e742363
Vik154 <Smorodin_1209@mail.ru>
2023-09-15 13:49:30 +0700

    ListView пример

--- 02_Элементы_управления/09_ListView/MainWindow.xaml.cs
+++ 02_Элементы_управления/09_ListView/MainWindow.xaml.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace _09_ListView {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _09_ListView;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
     }
+}
+
+public class EmployeeInfoDataSource {
+    public string? FirstName { get; set; }
+    public string? LastName  { get; set; }
+    public int Number {  get; set; }
 }
