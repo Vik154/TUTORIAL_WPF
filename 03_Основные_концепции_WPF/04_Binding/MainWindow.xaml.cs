c4f20e50f3ebd43bbe9b42d016e12d1f42176330
Vik154 <Smorodin_1209@mail.ru>
2023-09-18 13:49:42 +0700

    Создание привязки из кода

--- 03_Основные_концепции_WPF/04_Binding/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/04_Binding/MainWindow.xaml.cs
@@ -1,25 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _04_Binding {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _04_Binding;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        MakeBinding();
+    }
+
+    private void MakeBinding() {
+        StackPanel stackPanel     = new StackPanel();
+        Label      label          = new Label { FontSize = 20d, Content = "Binding" };
+        TextBox    sourceTextBox  = new TextBox { Margin = new Thickness(10) };
+        TextBox    reciverTextBox = new TextBox { Margin = new Thickness(10) };
+        
+        stackPanel.Children.Add(label);
+        stackPanel.Children.Add(sourceTextBox);
+        stackPanel.Children.Add(reciverTextBox);
+
+        Binding binding = new Binding();
+
+        // binding.ElementName = sourceTextBox.Name;
+        binding.Source = sourceTextBox;
+        binding.Path = new PropertyPath("Text");
+
+        reciverTextBox.SetBinding(TextBox.TextProperty, binding);
+
+        this.Content = stackPanel;
     }
 }
