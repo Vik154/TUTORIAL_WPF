5da7f26e91a19631c600e4d3765fc43278c2df75
Vik154 <Smorodin_1209@mail.ru>
2023-09-22 18:41:04 +0700

    неактивная кнопка

--- 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
@@ -7,14 +7,14 @@ public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
 
-        CommandBinding commandBinding = new CommandBinding(ApplicationCommands.Help);
-        commandBinding.Executed += new ExecutedRoutedEventHandler(MyLogical);
+        //CommandBinding commandBinding = new CommandBinding(ApplicationCommands.Help);
+        //commandBinding.Executed += new ExecutedRoutedEventHandler(MyLogical);
 
-        btn.CommandBindings.Add(commandBinding);
+        //btn.CommandBindings.Add(commandBinding);
     }
 
     // Обработчик для команды
-    private void MyLogical(object sender, ExecutedRoutedEventArgs e) {
-        txtInfo.Text += $"Вызвана команда HELP\nИсточник: {e.Source}\n";
-    }
+    //private void MyLogical(object sender, ExecutedRoutedEventArgs e) {
+    //    txtInfo.Text += $"Вызвана команда HELP\nИсточник: {e.Source}\n";
+    //}
 }
