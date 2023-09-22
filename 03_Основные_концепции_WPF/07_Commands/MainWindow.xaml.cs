baea4d1184576493535d70f7a2abbd6562f0f4d1
Vik154 <Smorodin_1209@mail.ru>
2023-09-22 22:50:18 +0700

    Работа со стандартными командами

--- 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
@@ -1,22 +1,62 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Controls;
 
 namespace _07_Commands;
 
 public partial class MainWindow : Window {
+
+    // Указывает изменился ли документ с момента открытия
+    private bool isDirty = false;
+    
     public MainWindow() {
         InitializeComponent();
 
+        CommandBinding commandBinding = new CommandBinding(ApplicationCommands.New);
+        commandBinding.Executed += NewCommand;
+        this.CommandBindings.Add(commandBinding);
+
+        commandBinding = new CommandBinding(ApplicationCommands.Open);
+        commandBinding.Executed += OpenCommand;
+        this.CommandBindings.Add(commandBinding);
+
+        commandBinding = new CommandBinding(ApplicationCommands.Save);
+        commandBinding.Executed += SaveCommand_Executed;
+        commandBinding.CanExecute += SaveCommand_CanExecute;
+        this.CommandBindings.Add(commandBinding);
+
         //CommandBinding commandBinding = new CommandBinding(ApplicationCommands.Help);
         //commandBinding.Executed += new ExecutedRoutedEventHandler(MyLogical);
-
         //btn.CommandBindings.Add(commandBinding);
     }
 
-
-    private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e) {
-        MessageBox.Show("Обработчик создан декларативно в XAML");
+    private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+        e.CanExecute = isDirty;
     }
+
+    private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e) {
+        MessageBox.Show($"Команда запущена с помощью {e.Source}");
+        isDirty = false;
+    }
+
+    private void OpenCommand(object sender, ExecutedRoutedEventArgs e) {
+        isDirty = false;
+    }
+
+    private void NewCommand(object sender, ExecutedRoutedEventArgs e) {
+        MessageBox.Show($"Команда запущена с помощью {e.Source}");
+        isDirty = false;
+    }
+
+    private void txt_TextChanged(object sender, TextChangedEventArgs e) {
+        isDirty = true;
+    }
+
+
+    //private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e) {
+    //    MessageBox.Show("Обработчик создан декларативно в XAML");
+    //}
 
     // Обработчик для команды
     //private void MyLogical(object sender, ExecutedRoutedEventArgs e) {
