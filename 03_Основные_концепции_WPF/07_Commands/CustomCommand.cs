7c2f8a3d1b56f9ff039cb69f980d3ed75a0419cb
Vik154 <Smorodin_1209@mail.ru>
2023-09-23 16:20:51 +0700

    Наследование от ICommand

---
+++ 03_Основные_концепции_WPF/07_Commands/CustomCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace _07_Commands;
+
+public class CustomCommand : ICommand {
+
+    // Когда срабатывает команда, метод CommandExecute (который в MainWindow Находится)
+    // будет сообщен с этим делегатом
+    private readonly Action<object>    _execute;
+
+    // Будет сообщен с методом CanCommandExecute (находится в MainWindow),
+    // который проверяет может ли команда выполняться
+    private readonly Predicate<object> _canExecute;
+
+    public CustomCommand(Action<object> execute, Predicate<object> canExecute) {
+
+        if (canExecute == null)
+            throw new ArgumentNullException("execute");
+
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public CustomCommand(Action<object> execute) : this(execute, null) { }
+
+    // Реализация события
+    public event EventHandler? CanExecuteChanged {
+        add    { CommandManager.RequerySuggested += value; }
+        remove {  CommandManager.RequerySuggested -= value;}
+    }
+
+    // Реализация метода интерфейса ICommand, возвращает true / false, т.е.
+    // Может ли быть выполнен метод Execute
+    public bool CanExecute(object? parameter) {
+        // Если _canExecute не равен null - вызывается делегат _canExecute
+        return _canExecute == null ? true : _canExecute.Invoke(parameter);
+    }
+
+    // Вызывает делегат Action с параметром parametr, т.е. метод CommandExecute,
+    // который определен в MainWindow
+    public void Execute(object? parameter) {
+        _execute.Invoke(parameter);
+    }
+}
--- 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/07_Commands/MainWindow.xaml.cs
@@ -8,11 +8,13 @@ namespace _07_Commands;
 public partial class MainWindow : Window {
 
     // Указывает изменился ли документ с момента открытия
-    private bool isDirty = false;
+    // Флаг - Для работы со стандартными командами
+    // private bool isDirty = false;
     
     public MainWindow() {
         InitializeComponent();
 
+        #region Стандартные команды WPF
         //CommandBinding commandBinding = new CommandBinding(ApplicationCommands.New);
         //commandBinding.Executed += NewCommand;
         //this.CommandBindings.Add(commandBinding);
@@ -25,23 +27,26 @@ public partial class MainWindow : Window {
         //commandBinding.Executed += SaveCommand_Executed;
         //commandBinding.CanExecute += SaveCommand_CanExecute;
         //this.CommandBindings.Add(commandBinding);
+        #endregion
 
+        #region Первый пример
         //CommandBinding commandBinding = new CommandBinding(ApplicationCommands.Help);
         //commandBinding.Executed += new ExecutedRoutedEventHandler(MyLogical);
         //btn.CommandBindings.Add(commandBinding);
+        #endregion
     }
 
+    #region MyCoomand
+    //private void Edit_Executed(object sender, ExecutedRoutedEventArgs e) {
+    //    txt.IsReadOnly = false;
+    //}
 
-    private void Edit_Executed(object sender, ExecutedRoutedEventArgs e) {
-        txt.IsReadOnly = false;
-    }
+    //private void Delete_Executed(object sender, ExecutedRoutedEventArgs e) {
+    //    txt.Clear();
+    //}
+    #endregion
 
-    private void Delete_Executed(object sender, ExecutedRoutedEventArgs e) {
-        txt.Clear();
-    }
-
-
-
+    #region Стандартные команды WPF
     //private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
     //    e.CanExecute = isDirty;
     //}
@@ -63,10 +68,9 @@ public partial class MainWindow : Window {
     //private void txt_TextChanged(object sender, TextChangedEventArgs e) {
     //    isDirty = true;
     //}
+    #endregion
 
-
-
-
+    #region Первый пример
     //private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e) {
     //    MessageBox.Show("Обработчик создан декларативно в XAML");
     //}
@@ -75,4 +79,22 @@ public partial class MainWindow : Window {
     //private void MyLogical(object sender, ExecutedRoutedEventArgs e) {
     //    txtInfo.Text += $"Вызвана команда HELP\nИсточник: {e.Source}\n";
     //}
+    #endregion
+
+    #region Создание команд с помощью ICommand
+
+    // Происходит при загрузке окна
+    private void Window_Loaded(object sender, RoutedEventArgs e) {
+        // К кнопке добавляется своя команда
+        _button.Command = new CustomCommand(CommandExecute, CanCommandExecute);
+    }
+
+    private void CommandExecute(object param) {
+        MessageBox.Show("Команда вызвана");
+    }
+
+    private bool CanCommandExecute(object param) {
+        return _txt.Text != string.Empty;
+    }
+    #endregion
 }
