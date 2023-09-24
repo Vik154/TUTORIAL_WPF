1262684378c609e36ac5129a6a1300b79700ac98
Vik154 <Smorodin_1209@mail.ru>
2023-09-24 23:23:54 +0700

    Пример обхода логического и визуального деревьев

--- 03_Основные_концепции_WPF/09_LVTrees/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/09_LVTrees/MainWindow.xaml.cs
@@ -1,21 +1,45 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _09_LVTrees {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace _09_LVTrees;
+
+public partial class MainWindow : Window {
+    
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    // Обход логического дерева
+    private void logicBtn_Click(object sender, RoutedEventArgs e) {
+        PrintLogicalTree(0, this);
+    }
+
+    // Обход визуального дерева
+    private void visualBtn_Click(object sender, RoutedEventArgs e) {
+        PrintVisualTree(0, this);
+    }
+
+    // Обход логического дерева
+    private void PrintLogicalTree(int depth, object obj) {
+        txt.Text += new string(' ', depth * 4) + $"<{obj.GetType().Name}>\n";
+
+        if (!(obj is DependencyObject)) 
+            return;
+
+        foreach (object child in LogicalTreeHelper.GetChildren(obj as DependencyObject))
+            PrintLogicalTree(depth + 1, child);
+    }
+
+    // Обход визуального дерева
+    private void PrintVisualTree(int depth, object obj) {
+
+        txt.Text += new string(' ', depth * 4) + $"<{obj.GetType().Name}>\n";
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj as DependencyObject); i++)
+            PrintVisualTree(depth + 1, VisualTreeHelper.GetChild(obj as DependencyObject, i));
+    }
+
+    private void ClearBtn_Click(object sender, RoutedEventArgs e) {
+        txt.Text = string.Empty;
     }
 }
