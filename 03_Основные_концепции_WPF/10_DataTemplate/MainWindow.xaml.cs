4f51ef306748faf0b7a394ec7a72983c2ce75abe
Vik154 <Smorodin_1209@mail.ru>
2023-09-25 21:48:37 +0700

    Работа с TreeView и вложенностью

--- 03_Основные_концепции_WPF/10_DataTemplate/MainWindow.xaml.cs
+++ 03_Основные_концепции_WPF/10_DataTemplate/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace _10_DataTemplate;
 
@@ -9,11 +10,12 @@ public partial class MainWindow : Window {
 
         List<Student> students = new List<Student> {
             new Student { Name = "Иван", SurName = "Иванович", ID = 1},
-            new Student { Name = "Иван", SurName = "Иванович", ID = 2},
+            new Student { Name = "Пётр", SurName = "Иванович", ID = 2},
             new Student { Name = "Иван", SurName = "Иванович", ID = 3}
         };
 
         _listStudents.ItemsSource = students;
+        _treeView.ItemsSource = new TreeViewPresenter().CityList;
     }
 }
 
@@ -22,3 +24,56 @@ public class Student {
     public string SurName { get; set; } = "";
     public int ID { get; set; }
 }
+
+/*----------------------------------------------------------------------*/
+/* Для TreeView */
+public partial class City {
+    public string CityName { get; set; } = "";
+    public ObservableCollection<Street> StreetsList { get; set; } = default!;
+}
+
+public partial class Street {
+    public string StreetName { get; set; } = "";
+    public int[] HouseNumber { get; set; } = { 1,2,3 };
+}
+
+
+public partial class TreeViewPresenter : Window {
+    public ObservableCollection<City> CityList { get; set; }
+
+    public TreeViewPresenter() {
+        CityList = new ObservableCollection<City> {
+
+            new City {
+                CityName = "Москва",
+                StreetsList = new ObservableCollection<Street> {
+                    new Street { StreetName = "Державина"},
+                    new Street { StreetName = "Советская"},
+                    new Street { StreetName = "Кирова"},
+                    new Street { StreetName = "Пролетарская"}
+                },
+            },
+
+            new City {
+                CityName = "Омск",
+                StreetsList = new ObservableCollection<Street> {
+                    new Street { StreetName = "Державина"},
+                    new Street { StreetName = "Советская"},
+                    new Street { StreetName = "Кирова"},
+                    new Street { StreetName = "Пролетарская"}
+                },
+            },
+
+            new City {
+                CityName = "Томск",
+                StreetsList = new ObservableCollection<Street> {
+                    new Street { StreetName = "Державина"},
+                    new Street { StreetName = "Советская"},
+                    new Street { StreetName = "Кирова"},
+                    new Street { StreetName = "Пролетарская"}
+                },
+            }
+        };
+    }
+}
+/*----------------------------------------------------------------------*/
