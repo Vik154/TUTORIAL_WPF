aa37b55a1334ad0433db38a565e07f46493b9597
Vik154 <Smorodin_1209@mail.ru>
2023-09-27 01:39:59 +0700

    Тест модели - представления

--- Проект_1/StartUpMVVM/ViewModels/MainWindowViewModel.cs
+++ Проект_1/StartUpMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StartUpMVVM.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,24 @@ using System.Threading.Tasks;
 
 namespace StartUpMVVM.ViewModels;
 
-class MainWindowViewModel {
+internal class MainWindowViewModel : ViewModel {
+
+    #region Заголовок Окна
+
+    private string _Title = "Текст из ViewModel";
+
+    /// <summary> Заголовок окна </summary>
+    public string Title { 
+        get => _Title;
+        set => Set(ref _Title, value);
+        
+        // set {
+        //      if (Equals(_Title, value))
+        //          return;
+        //      _Title = value;
+        //      OnPropertyChanged();
+        // }
+    }
+    #endregion
 
 }
