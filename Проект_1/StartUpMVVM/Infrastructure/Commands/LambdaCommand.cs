a9fe14a55f69409a70ea790a0511f1826ac1ab55
Vik154 <Smorodin_1209@mail.ru>
2023-09-27 12:40:33 +0700

    Реализация команды

---
+++ Проект_1/StartUpMVVM/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using StartUpMVVM.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StartUpMVVM.Infrastructure.Commands;
+
+// Подобие RelayCommand из MVVM Light
+internal class LambdaCommand : Command {
+
+    private readonly Action<object> _Execute;
+    private readonly Func<object, bool> _CanExecute;
+
+    // Получает 2 действия, которые команда может выполнять
+    public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null) {
+        _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+        _CanExecute = CanExecute;
+    }
+
+    public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+    public override void Execute(object parameter)    => _Execute(parameter);
+}
