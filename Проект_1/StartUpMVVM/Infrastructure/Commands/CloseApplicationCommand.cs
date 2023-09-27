1e34e27c4438d907aa5f4ac953d466ad5fc0b57b
Vik154 <Smorodin_1209@mail.ru>
2023-09-27 12:56:35 +0700

    Вынос команд в отдельные классы

---
+++ Проект_1/StartUpMVVM/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using StartUpMVVM.Infrastructure.Commands.Base;
+using System.Windows;
+
+
+namespace StartUpMVVM.Infrastructure.Commands;
+
+// Вынос команд в отдельные классы
+internal class CloseApplicationCommand : Command {
+    public override bool CanExecute(object parameter) => true;
+    public override void Execute(object parameter) => Application.Current.Shutdown();
+}
