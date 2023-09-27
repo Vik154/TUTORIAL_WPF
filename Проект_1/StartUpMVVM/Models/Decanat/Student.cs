90d014d8b6138ff763f81c7ee4c9cb422d26d815
Vik154 <Smorodin_1209@mail.ru>
2023-09-27 22:35:59 +0700

    Надстройка DataGrid

--- Проект_1/StartUpMVVM/Models/Decanat/Student.cs
+++ Проект_1/StartUpMVVM/Models/Decanat/Student.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 namespace StartUpMVVM.Models.Decanat;
 
 internal class Student {
-
     public string Name { get; set; }
     public string SurName {  get; set; }
     public string Patronymic {  get; set; }
     public DateTime Birthday { get; set; }
     public double Rating {  get; set; }
+    public string Description { get; set; }
 }
 
 internal class Group {
     public string Name { get; set; }
     public ICollection<Student> Students { get; set; }
+    public string Description { get; set; }
 }
