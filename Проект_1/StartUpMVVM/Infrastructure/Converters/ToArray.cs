b533acd23ac26692e3f9648bc88162a9fe745c31
Vik154 <Smorodin_1209@mail.ru>
2023-09-30 23:58:49 +0700

    Пример мультиконвертера

---
+++ Проект_1/StartUpMVVM/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+
+namespace StartUpMVVM.Infrastructure.Converters;
+
+
+internal class ToArray : MultiConverter {
+
+    public override object Convert(object[] vv, Type t, object p, CultureInfo c) {
+        var collection = new CompositeCollection();
+        foreach (var value in vv)
+            collection.Add(value);
+        return collection;
+    }
+
+    //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => v as object[];
+}
