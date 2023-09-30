57867594abc412f7bb035219104425e512174fa8
Vik154 <Smorodin_1209@mail.ru>
2023-09-30 15:42:43 +0700

    Базовая логика модели-представления статистики

--- Проект_1/StartUpMVVM/ViewModels/CountriesStatisticViewModel.cs
+++ Проект_1/StartUpMVVM/ViewModels/CountriesStatisticViewModel.cs
@@ -1,21 +1,48 @@
-﻿using StartUpMVVM.Services;
+﻿using StartUpMVVM.Infrastructure.Commands;
+using StartUpMVVM.Models;
+using StartUpMVVM.Services;
 using StartUpMVVM.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace StartUpMVVM.ViewModels;
 
 internal class CountriesStatisticViewModel : ViewModel {
 
     private DataService _dataService;
-
     private MainWindowViewModel MainModel { get; }
+
+    #region _Countries - Статистика по странам
+
+    /// <summary> Статистика по странам </summary>
+    private IEnumerable<CountryInfo> _Countries;
+
+    /// <summary> Статистика по странам </summary>
+    public IEnumerable<CountryInfo> Countries {
+        get => _Countries;
+        private set => Set(ref _Countries, value);
+    }
+
+    #endregion
+
+    #region Команды
+
+    public ICommand RefreshDataCommand { get; }
+
+    private void OnRefreshDataCommandExecuted(object p) {
+        Countries = _dataService.GetData();            
+    }
+
+    #endregion
 
     public CountriesStatisticViewModel(MainWindowViewModel mainModel) {
         MainModel = mainModel;
         _dataService = new DataService();
+
+        RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
     }
 }
