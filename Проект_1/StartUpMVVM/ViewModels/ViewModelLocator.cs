8be689e4c256cba3460b8ce5b749a268c7b039d2
Vik154 <Smorodin_1209@mail.ru>
2023-10-01 17:22:10 +0700

    Создание ViewModelLocator

---
+++ Проект_1/StartUpMVVM/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace StartUpMVVM.ViewModels;
+
+// Класс - за счет которого осуществляется доступ к определенным ViewModels
+internal class ViewModelLocator {
+
+    public MainWindowViewModel MainWindowModel => 
+        App.Host.Services.GetRequiredService<MainWindowViewModel>();
+
+    //public StudentsManagementViewModel StudentManagement => 
+    //    App.Host.Services.GetRequiredService<StudentsManagementViewModel>();
+}
