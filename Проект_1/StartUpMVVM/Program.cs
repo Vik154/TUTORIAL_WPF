a274288955ab955d2bb2d2f69039636627f20a9e
Vik154 <Smorodin_1209@mail.ru>
2023-10-01 15:30:24 +0700

    Класс program изменение точки входа

---
+++ Проект_1/StartUpMVVM/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StartUpMVVM;
+
+public static class Program {
+
+    [STAThread]
+    public static void Main(string[] args) {
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
