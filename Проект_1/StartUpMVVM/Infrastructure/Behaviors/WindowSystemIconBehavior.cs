1e73e69b9ca274d759ad207ff4cff2d16aa5ae0a
Vik154 <Smorodin_1209@mail.ru>
2023-10-03 20:54:12 +0700

    Создание системного меню winapi - style

---
+++ Проект_1/StartUpMVVM/Infrastructure/Behaviors/WindowSystemIconBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+using StartUpMVVM.Infrastructure.Extensions;
+
+namespace StartUpMVVM.Infrastructure.Behaviors;
+
+class WindowSystemIconBehavior : Behavior<Image> {
+    protected override void OnAttached() {
+        AssociatedObject.MouseLeftButtonDown += OnMouseDown;
+    }
+
+    protected override void OnDetaching() {
+        AssociatedObject.MouseLeftButtonDown -= OnMouseDown;
+    }
+
+    private void OnMouseDown(object Sender, MouseButtonEventArgs E) {
+        if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+
+        E.Handled = true;
+
+        if (E.ClickCount > 1)
+            window.Close();
+        else
+            window.SendMessage(WM.SYSCOMMAND, SC.KEYMENU);
+    }
+}
