a11d0ef38f03b35b67f6a6bac3b7069723bb7c79
Vik154 <Smorodin_1209@mail.ru>
2023-10-03 19:19:37 +0700

    Добавление поведения для системных кнопок

---
+++ Проект_1/StartUpMVVM/Infrastructure/Behaviors/MinimizeWindow.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StartUpMVVM.Infrastructure.Behaviors;
+
+
+class MinimizeWindow : Behavior<Button> {
+    protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+    protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+    private void OnButtonClick(object Sender, RoutedEventArgs E) {
+        if (!(AssociatedObject.FindVisualRoot() is Window window)) 
+            return;
+        window.WindowState = WindowState.Minimized;
+    }
+}
---
+++ Проект_1/StartUpMVVM/Infrastructure/Behaviors/WindowStateChange.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StartUpMVVM.Infrastructure.Behaviors;
+
+class WindowStateChange : Behavior<Button> {
+
+    protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+    protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+    private void OnButtonClick(object Sender, RoutedEventArgs E) {
+        if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+
+        //switch (window.WindowState)
+        //{
+        //    case WindowState.Normal:
+        //        window.WindowState = WindowState.Maximized;
+        //        break;
+        //    case WindowState.Maximized:
+        //        window.WindowState = WindowState.Normal;
+        //        break;
+        //}
+
+        window.WindowState = window.WindowState switch {
+            WindowState.Normal => WindowState.Maximized,
+            WindowState.Maximized => WindowState.Normal,
+            _ => window.WindowState
+        };
+    }
+}
