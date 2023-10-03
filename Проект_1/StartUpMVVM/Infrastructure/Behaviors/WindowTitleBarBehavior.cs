1f26b8d4e71004665c6934de6fe688f57243d788
Vik154 <Smorodin_1209@mail.ru>
2023-10-03 20:14:44 +0700

    Реализация логики поведения при перемещении окна

---
+++ Проект_1/StartUpMVVM/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StartUpMVVM.Infrastructure.Behaviors;
+
+class WindowTitleBarBehavior : Behavior<UIElement> {
+    
+    private Window? _Window;
+
+    protected override void OnAttached() {
+        _Window = AssociatedObject as Window ?? AssociatedObject.FindLogicalParent<Window>();
+        AssociatedObject.MouseLeftButtonDown += OnMouseDown;
+    }
+
+    protected override void OnDetaching() {
+        AssociatedObject.MouseLeftButtonDown -= OnMouseDown;
+        _Window = null;
+    }
+
+    private void OnMouseDown(object Sender, MouseButtonEventArgs E) {
+        switch (E.ClickCount) {
+            case 1:
+                DragMove();
+                break;
+            default:
+                Maximize();
+                break;
+        }
+    }
+
+    private void DragMove() {
+        if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+        window.DragMove();
+    }
+
+    private void Maximize() {
+        if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+        window.WindowState = window.WindowState switch {
+            WindowState.Normal => WindowState.Maximized,
+            WindowState.Maximized => WindowState.Normal,
+            _ => window.WindowState
+        };
+    }
+}
