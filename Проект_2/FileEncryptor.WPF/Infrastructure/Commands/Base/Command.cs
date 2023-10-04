cf03d5bd3fb05c6c537cf303496123e1d6508c09
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 21:22:21 +0700

    Базовая логика команды

---
+++ Проект_2/FileEncryptor.WPF/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands.Base;
+
+// Базовая логика команды
+internal abstract class Command : ICommand {
+
+    public event EventHandler? CanExecuteChanged {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    // Дополнительное свойство - которое включает или выключает команду целиком
+    private bool _Executable;
+    public bool Executable {
+        get => _Executable;
+        set {
+            if (_Executable == value)
+                return;
+            _Executable = value;
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
+    // Неявная реализация методов команды (позволяет создать дополнительное свойства)
+    bool ICommand.CanExecute(object? parameter) => _Executable && CanExecute(parameter);
+    
+    void ICommand.Execute(object? parameter) {
+        if (CanExecute(parameter))      // Если команду можно выполнить - тогда выполнить
+            Execute(parameter);         // помогает избавиться от лишних проверок в наследниках
+    }
+
+    // Всегда возвращает true, если не переопределен наследником
+    protected virtual bool CanExecute(object? parameter) => true;
+
+    // Наследник должен реализовать собственную логику этого метода
+    protected abstract void Execute(object? parameter);
+}
