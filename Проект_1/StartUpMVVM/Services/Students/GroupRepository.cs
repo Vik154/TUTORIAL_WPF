24339441f18227a0a19afc4cae82627fbab53f94
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 13:30:30 +0700

    Классы хранения данных о студентах

--- Проект_1/StartUpMVVM/Services/Students/GroupRepository.cs
+++ Проект_1/StartUpMVVM/Services/Students/GroupRepository.cs
@@ -4,6 +4,11 @@ using StartUpMVVM.Services.Base;
 namespace StartUpMVVM.Services.Students;
 
 class GroupRepository : RepositoryInMemory<Group> {
+    
+    public GroupRepository() : base(TestData.Groups) { }
+
+    public Group? Get(string GroupName) => 
+        GetAll().FirstOrDefault(g => g.Name == GroupName);
 
     protected override void Update(Group Source, Group Destination) {
         Destination.Name = Source.Name;
--- Проект_1/StartUpMVVM/Services/Students/StudentsRepository.cs
+++ Проект_1/StartUpMVVM/Services/Students/StudentsRepository.cs
@@ -5,6 +5,8 @@ namespace StartUpMVVM.Services.Students;
 
 class StudentsRepository : RepositoryInMemory<Student> {
 
+    public StudentsRepository() : base(TestData.Students) { }
+
     protected override void Update(Student Source, Student Destination) {
         Destination.Name = Source.Name;
         Destination.SurName = Source.SurName;
