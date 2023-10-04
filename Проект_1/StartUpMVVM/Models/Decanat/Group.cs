bbef5dbc6194c7195a0f915f979eb4f69444cc7a
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 14:01:10 +0700

    Работа с ошибками, отлов искдючений

--- Проект_1/StartUpMVVM/Models/Decanat/Group.cs
+++ Проект_1/StartUpMVVM/Models/Decanat/Group.cs
@@ -5,6 +5,6 @@ namespace StartUpMVVM.Models.Decanat;
 internal class Group : IEntity {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public IList<Student>? Students { get; set; }
+    public IList<Student>? Students { get; set; } = new List<Student>();
     public string? Description { get; set; }
 }
