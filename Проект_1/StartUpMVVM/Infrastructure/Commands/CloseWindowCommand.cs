b5468f7f80312f76c28249455b76bab5edbb14d8
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 15:48:47 +0700

    Команда закрытия окна

---
+++ Проект_1/StartUpMVVM/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,31 @@
+﻿using StartUpMVVM.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace StartUpMVVM.Infrastructure.Commands;
+
+class CloseWindowCommand : Command {
+    public override bool CanExecute(object? parameter) => parameter is Window;
+
+    public override void Execute(object? parameter) {
+        if (!CanExecute(parameter) || parameter is null) 
+            return;
+
+        var window = (Window)parameter;
+        window.Close();
+    }
+}
+
+class CloseDialogCommand : Command {
+    public bool? DialogResult { get; set; }
+
+    public override bool CanExecute(object? parameter) => parameter is Window;
+
+    public override void Execute(object? parameter) {
+        if (!CanExecute(parameter) || parameter is null) 
+            return;
+
+        var window = (Window)parameter;
+        window.DialogResult = DialogResult;
+        window.Close();
+    }
+}
