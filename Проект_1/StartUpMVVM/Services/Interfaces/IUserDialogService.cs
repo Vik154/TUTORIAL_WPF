24041c2a71416a7561fcfb41f34987d5ac265500
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 15:48:00 +0700

    Добавление интерфейса для диалогового окна редактирования пользователя

---
+++ Проект_1/StartUpMVVM/Services/Interfaces/IUserDialogService.cs
@@ -0,0 +1,10 @@
+﻿namespace StartUpMVVM.Services.Interfaces;
+
+interface IUserDialogService {
+    bool Edit(object item);
+    void ShowInformation(string Information, string Caption);
+    void ShowWarning(string Message, string Caption);
+    void ShowError(string Message, string Caption);
+    bool Confirm(string Message, string Caption, bool Exclamation = false);
+    string GetStringValue(string Message, string Caption, string? DefaultValue = null);
+}
