80de97e10c1ea94f44044513f81701f90c401af3
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 11:51:30 +0700

    Создание модели-представления для управления студентами из модального окна

---
+++ Проект_1/StartUpMVVM/ViewModels/StudentsManagementViewModel.cs
@@ -0,0 +1,17 @@
+﻿using StartUpMVVM.ViewModels.Base;
+
+namespace StartUpMVVM.ViewModels;
+
+class StudentsManagementViewModel : ViewModel {
+
+    #region Title - заголовок окна
+    /// <summary> Заголовок окна </summary>
+    private string _Title = "Управление студентами";
+
+    /// <summary> Заголовок окна </summary>
+    public string Title {
+        get => _Title;
+        set => Set(ref _Title, value);
+    }
+    #endregion
+}
