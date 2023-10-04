6b2b521a7a0d2c21a60e1f187d4737e2cd243ffd
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 11:49:11 +0700

    Команда открытия окна для управления студентами

---
+++ Проект_1/StartUpMVVM/Infrastructure/Commands/ManageStudentsCommand.cs
@@ -0,0 +1,28 @@
+﻿using StartUpMVVM.Infrastructure.Commands.Base;
+using StartUpMVVM.Views.Windows;
+using System.Windows;
+
+namespace StartUpMVVM.Infrastructure.Commands;
+
+class ManageStudentsCommand : Command {
+
+    private StudentsManagementWindow? _Window;
+
+    public override bool CanExecute(object? parameter) => _Window == null;
+
+    public override void Execute(object? parameter) {
+        var window = new StudentsManagementWindow {
+            Owner = Application.Current.MainWindow
+        };
+        _Window = window;
+        window.Closed += OnWindowClosed;
+
+        window.ShowDialog();
+    }
+
+    private void OnWindowClosed(object? sender, EventArgs e) {
+        if (sender != null)
+            ((Window)sender).Closed -= OnWindowClosed;
+        _Window = null;
+    }
+}
