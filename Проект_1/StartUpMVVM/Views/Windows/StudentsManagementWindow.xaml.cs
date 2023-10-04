cbe1dce62b2c47976ea4f13435cd83c0ea75cfa5
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 11:52:29 +0700

    Модальное окно для управления студентами

---
+++ Проект_1/StartUpMVVM/Views/Windows/StudentsManagementWindow.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace StartUpMVVM.Views.Windows;
+
+public partial class StudentsManagementWindow : Window {
+    public StudentsManagementWindow() => InitializeComponent();
+}
