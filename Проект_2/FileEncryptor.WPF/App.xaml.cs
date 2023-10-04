7def6a0b0fc39ecd841ec56de4d6ce6d6d735d97
Vik154 <Smorodin_1209@mail.ru>
2023-10-04 22:10:46 +0700

    Реализация логики хоста

--- Проект_2/FileEncryptor.WPF/App.xaml.cs
+++ Проект_2/FileEncryptor.WPF/App.xaml.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Configuration;
-using System.Data;
 using System.Windows;
 
-namespace FileEncryptor.WPF; 
+namespace FileEncryptor.WPF;
 
-public partial class App : Application {
-    
-    internal static void ConfigureServices(HostBuilderContext context, IServiceCollection collection) {
-        throw new NotImplementedException();
+public partial class App {
+
+    // Ленивый хост
+    private static IHost? __Host;
+
+    public static IHost Host => __Host ??= Program
+        .CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    internal static void ConfigureServices(HostBuilderContext host, 
+                                           IServiceCollection services) {    
+        
+    }
+
+    protected override async void OnStartup(StartupEventArgs e) {
+        var host = Host;
+        base.OnStartup(e);
+        await host.StartAsync();
+    }
+
+    protected override async void OnExit(ExitEventArgs e) {
+        base.OnExit(e);
+        using (Host)
+            await Host.StopAsync();
     }
 }
