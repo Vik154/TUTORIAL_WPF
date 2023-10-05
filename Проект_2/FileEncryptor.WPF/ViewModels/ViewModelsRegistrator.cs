32554748349f77b5a0da4355c69e2ceef3e812f5
Vik154 <Smorodin_1209@mail.ru>
2023-10-05 13:42:42 +0700

    Регистрация модели главного окна

---
+++ Проект_2/FileEncryptor.WPF/ViewModels/ViewModelsRegistrator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FileEncryptor.WPF.ViewModels;
+
+// Регистрация всех моделей представления
+internal static class ViewModelsRegistrator {
+
+    // Регистрация модели главного окна  
+    public static IServiceCollection AddViewModels(this IServiceCollection services) =>
+        services.AddSingleton<MainWindowViewModel>();
+}
