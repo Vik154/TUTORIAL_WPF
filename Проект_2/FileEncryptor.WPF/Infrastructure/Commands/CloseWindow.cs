ff1f08a17a8e6e6c8d990ad8d799b6bacf44c08f
Vik154 <Smorodin_1209@mail.ru>
2023-10-05 16:50:51 +0700

    Команда закрытия окна

--- Проект_2/FileEncryptor.WPF/Infrastructure/Commands/CloseWindow.cs
+++ Проект_2/FileEncryptor.WPF/Infrastructure/Commands/CloseWindow.cs
@@ -3,7 +3,11 @@ using System.Windows;
 
 namespace FileEncryptor.WPF.Infrastructure.Commands;
 
+// Команда закрытия окна
 internal class CloseWindow : Command {
+
+    protected override bool CanExecute(object? parameter) =>
+        (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow) != null;
 
     protected override void Execute(object? parameter) =>
         (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow)?.Close();
