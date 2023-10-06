7bc226a5776d77a52c78e803452f2aa261a09b29
Vik154 <Smorodin_1209@mail.ru>
2023-10-06 20:11:43 +0700

    Добавление ViewModel и свойства заголовка окна

---
+++ Проект_3/Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MathCore.ViewModels;
+
+
+namespace Bookinist.ViewModels; 
+
+class MainWindowViewModel : ViewModel {
+
+    #region Title - заголовок окна
+    /// <summary> Заголовок окна </summary>
+    private string? _Title;
+
+    /// <summary> Заголовок окна </summary>
+    public string? Title {
+        get => _Title;
+        set => Set(ref _Title, value);
+    }
+    #endregion
+}
