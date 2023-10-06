b76e355448dd223989d5c50bb699ec75f3f8da1c
Vik154 <Smorodin_1209@mail.ru>
2023-10-07 02:06:48 +0700

    Загатовка для реализации интерфейса IRepository

---
+++ Проект_3/Bookinist.DAL/DbRepository.cs
@@ -0,0 +1,40 @@
+﻿using Bookinist.DAL.Entityes.Base;
+using Bookinist.Interfaces;
+
+namespace Bookinist.DAL;
+
+class DbRepository<T> : IRepository<T> where T : Entity, new() {
+    public IEnumerable<T> Items => throw new NotImplementedException();
+
+    public T Add(T item) {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> AddAsync(T item, CancellationToken Cancel = default) {
+        throw new NotImplementedException();
+    }
+
+    public T Get(int id) {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetAsync(int id, CancellationToken Cancel = default) {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(int id) {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(int id, CancellationToken Cancel = default) {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T item) {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T item, CancellationToken Cancel = default) {
+        throw new NotImplementedException();
+    }
+}
