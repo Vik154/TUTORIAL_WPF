0549928487be38c42de22f78d882ff23f973f666
Vik154 <Smorodin_1209@mail.ru>
2023-10-07 01:53:25 +0700

    Создание интерфейсов сущностей и репозитория в отдельной библиотеке

---
+++ Проект_3/Bookinist.Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.Interfaces;
+
+public interface IEntity {
+    int Id { get; set; }
+}
---
+++ Проект_3/Bookinist.Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.Interfaces;
+
+// Шаблонный интерфейс, который имеет ограничение на шаблон,
+// он должен быть классом, иметь базовый конструктор и наследовать IEntity
+public interface IRepository<T> where T : class, IEntity, new() {
+    IEnumerable<T> Items {  get; }
+    T Get(int id);
+    T Add(T item);
+    void Update(T item);
+    void Remove(int id);
+}
