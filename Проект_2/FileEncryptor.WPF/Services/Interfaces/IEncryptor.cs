c4d16e0e8b9cefaba0d73eb8d12283143f1c3948
Vik154 <Smorodin_1209@mail.ru>
2023-10-06 13:32:31 +0700

    Интерфейс шифратора файлов

--- Проект_2/FileEncryptor.WPF/Services/Interfaces/IEncryptor.cs
+++ Проект_2/FileEncryptor.WPF/Services/Interfaces/IEncryptor.cs
@@ -2,12 +2,16 @@
 
 // Интерфейс шифратора файлов
 internal interface IEncryptor {
+
     // Шифрует файл по указанному пути
-    void Encrypt(string SourcePath, string DestinationPath, string Password, int BufferLength = 104200);
+    void Encrypt(string SourcePath, string DestinationPath, 
+                 string Password, int BufferLength = 104200);
     
     // Дешифрация файла с возвратом успешного / не успешного значения
-    bool Decrypt(string SourcePath, string DestinationPath, string Password, int BufferLength = 104200);
+    bool Decrypt(string SourcePath, string DestinationPath, 
+                 string Password, int BufferLength = 104200);
 
+    // Асинхронные операции шифровки / дешифровки файла
     Task EncryptAsync(string SourcePath, string DestinationPath, 
                       string Password, int BufferLength = 104200, 
                       IProgress<double>? Progress = null, 
