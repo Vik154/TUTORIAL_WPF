e0271fb208d740d38cd1eb7235979cfa05b530db
Vik154 <Smorodin_1209@mail.ru>
2023-10-06 21:42:05 +0700

    Контейнер сервисов и точка входа

--- Проект_3/Bookinist/App.xaml.cs
+++ Проект_3/Bookinist/App.xaml.cs
@@ -1,12 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
-namespace Bookinist {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
+namespace Bookinist;
+
+public partial class App : Application {
+
+    private static IHost __Host;
+
+    public static IHost Host =>__Host 
+        ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    public static IServiceProvider Services =>__Host.Services;
+
+    internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services) {
+
     }
 
+    protected override async void OnStartup(StartupEventArgs e) {
+        var host = Host;
+        base.OnStartup(e);
+        await host.StartAsync();
+    }
+
+    protected override async void OnExit(ExitEventArgs e) {
+        using var host = Host;
+        base.OnExit(e);
+        await host.StopAsync();
+    }
 }
---
+++ Проект_3/Bookinist/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist;
+
+public class Program {
+
+    [STAThread]
+    public static void Main(string[] args) {
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+
+    // CreateHostBuilder - это название фиксировано для EF
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureServices(App.ConfigureServices);
+        
+}
