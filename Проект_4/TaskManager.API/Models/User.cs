d9674e45b8a3b3b8aaca3e62e294f3bc4e0da9c5
Vik154 <Smorodin_1209@mail.ru>
2023-10-11 21:51:33 +0700

    Создание моделей

---
+++ Проект_4/TaskManager.API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.API.Models;
+
+public class User {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public DateTime LastLoginDate { get; set; }
+    public byte[] Photo { get; set; }
+}
