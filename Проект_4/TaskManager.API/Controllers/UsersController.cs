37812293c903d06b198c032983aad11af380242a
Vik154 <Smorodin_1209@mail.ru>
2023-10-12 00:13:15 +0700

    Добавление контроллера

--- Проект_4/TaskManager.API/Controllers/UsersController.cs
+++ Проект_4/TaskManager.API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskManager.API.Models;
 using TaskManager.API.Models.Data;
+using TaskManager.Common.Models;
 
 namespace TaskManager.API.Controllers;
 
@@ -14,4 +16,15 @@ public class UsersController : ControllerBase {
     // Ctor
     public UsersController(ApplicationContext db) => _db = db;
 
+    [HttpPost("create")]
+    public IActionResult CreateUser([FromBody] UserModel user) {
+        if (user is null)
+            return BadRequest("Не корректный юзер");
+
+        User newUser = new User(user.FirstName, user.LastName, user.Email,
+            user.Password, user.Status, user.Phone, user.Photo);
+        _db.Users.Add(newUser);
+        _db.SaveChanges();
+        return Ok();
+    }
 }
