7ca89010d7d4274e55d4d2f0d1708ae4a92f0b79
Vik154 <Smorodin_1209@mail.ru>
2023-10-13 20:26:07 +0700

    Создание дата контекста и реализация логики добавления отдела

--- Проект_5/ManageDB/View/AddNewDepartmentWindow.xaml.cs
+++ Проект_5/ManageDB/View/AddNewDepartmentWindow.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ManageDB.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace ManageDB.View {
-    /// <summary>
-    /// Логика взаимодействия для AddNewDepartmentWindow.xaml
-    /// </summary>
-    public partial class AddNewDepartmentWindow : Window {
-        public AddNewDepartmentWindow() {
-            InitializeComponent();
-        }
+namespace ManageDB.View;
+
+
+public partial class AddNewDepartmentWindow : Window {
+    public AddNewDepartmentWindow() {
+        InitializeComponent();
+        //DataContext = new DataManageVM();
     }
 }
