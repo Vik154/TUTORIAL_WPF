2388b469706048e652f59b9cc67cd5fb3e14b38f
Vik154 <Smorodin_1209@mail.ru>
2023-10-13 21:23:37 +0700

    Логика добавления новых позиций

--- Проект_5/ManageDB/ViewModel/DataManageVM.cs
+++ Проект_5/ManageDB/ViewModel/DataManageVM.cs
@@ -10,6 +10,7 @@ namespace ManageDB.ViewModel;
 
 internal class DataManageVM : INotifyPropertyChanged {
 
+    #region СВОЙСТВА - ЗАВИСИМОСТИ
     public event PropertyChangedEventHandler? PropertyChanged;
 
     private void OnPropertyChanged(string propertyName) {
@@ -50,11 +51,30 @@ internal class DataManageVM : INotifyPropertyChanged {
         }
     }
 
-    // Имя отдела
+    #endregion
+
+    #region СВОЙСТВА
+
+    // Свойства для отдела
     public string? DepartmentName { get; set; }
 
+    // Свойства для позиции
+    public string? PositionName { get; set; }
+    public decimal PositionSalary { get; set; }
+    public int PositionMaxNumber { get; set; }
+    public Department? PositionDepartment { get; set; }
+
+    // Свойства для пользователей
+    public string? UserName { get; set; }
+    public string? UserSurName { get; set; }
+    public string? UserPhone { get; set; }
+    public Position? UserPosition { get; set; }
+
+    #endregion
+
     #region КОМАНДЫ ОТКРЫТИЯ ОКОН ДЛЯ ДОБАВЛЕНИЯ НОВЫХ ЭЛЕМЕНТОВ
-    
+
+    // Команда для добавления отделов
     private RelayCommand? addNewDepartment;
     public RelayCommand AddNewDepartment {
         get {
@@ -79,9 +99,43 @@ internal class DataManageVM : INotifyPropertyChanged {
         }
     }
 
+    // Команда для добавления позиций
+    private RelayCommand? addNewPosition;
+    public RelayCommand AddNewPosition {
+        get {
+            return addNewPosition ?? new RelayCommand(obj => {
+                Window? window = obj as Window;
+                string res = "";
+
+                if (string.IsNullOrEmpty(PositionName) ||
+                    PositionName.Replace(" ", "").Length == 0) {
+                    SetRedBlockControl(window!, "NameBlock");
+                }
+                
+                if (PositionSalary == 0)
+                    SetRedBlockControl(window!, "SalaryBlock");
+                
+                if (PositionMaxNumber == 0)
+                    SetRedBlockControl(window!, "MaxNumberBlock");
+
+                if (PositionDepartment is null)
+                    MessageBox.Show("Укажите отдел");
+
+                else {
+                    res = DataWorker.CreatePosition(PositionName!, PositionSalary, 
+                                     PositionMaxNumber, PositionDepartment);
+                    UpdateAllDataView();
+                    ShowMessageToUser(res);
+                    SetNullValuesToProperties();
+                    window?.Close();
+                }
+            });
+        }
+    }
+
     #endregion
 
-    #region КОМАНДЫ ОТКРЫТИЯ ОКОН
+        #region КОМАНДЫ ОТКРЫТИЯ ОКОН
     private RelayCommand? openAddNewDepartmentWnd;
     public RelayCommand OpenAddNewDepartmentWnd {
         get => openAddNewDepartmentWnd ?? new RelayCommand(obj
@@ -198,6 +252,13 @@ internal class DataManageVM : INotifyPropertyChanged {
     /// <summary> Обнуление полей при изменениях </summary>
     private void SetNullValuesToProperties() {
 
+        // Обнуление ссылок для позиций
+        PositionName = null;
+        PositionSalary = 0;
+        PositionMaxNumber = 0;
+        PositionDepartment = null;
+
+        // Обнуление ссылок для отдела
         DepartmentName = null;
     }
 
