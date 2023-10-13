398e32559f94999440aced89341b9be39aa1d881
Vik154 <Smorodin_1209@mail.ru>
2023-10-13 20:27:11 +0700

    Логика оповещения пользователя

--- Проект_5/ManageDB/View/MessageView.xaml.cs
+++ Проект_5/ManageDB/View/MessageView.xaml.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace ManageDB.View {
-    /// <summary>
-    /// Логика взаимодействия для MessageView.xaml
-    /// </summary>
-    public partial class MessageView : Window {
-        public MessageView() {
-            InitializeComponent();
-        }
+namespace ManageDB.View;
+
+// Кривая логика ! так делать не надо (надо через view model)
+public partial class MessageView : Window {
+
+    public MessageView(string message) {
+        InitializeComponent();
+        MessageText.Text = message;
+    }
+
+    private void ButtonClose_Click(object sender, RoutedEventArgs e) {
+        this.Close();
     }
 }
