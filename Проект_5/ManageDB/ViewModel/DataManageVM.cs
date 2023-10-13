6307b5b25a20d4b40f0b5baee8f6c39b68ca55ac
Vik154 <Smorodin_1209@mail.ru>
2023-10-13 13:43:54 +0700

    Класс для взаимодействия с базой данных

--- Проект_5/ManageDB/ViewModel/DataManageVM.cs
+++ Проект_5/ManageDB/ViewModel/DataManageVM.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ManageDB.Model;
+using System.ComponentModel;
 
-namespace ManageDB.ViewModel {
-    internal class DataManageVM {
+namespace ManageDB.ViewModel;
+
+
+internal class DataManageVM : INotifyPropertyChanged {
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName) {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    // Все отделы
+    private List<Department> allDepartments = DataWorker.GetAllDepartments();
+
+    public List<Department> AllDepartments {
+        get => allDepartments;
+        set {
+            allDepartments = value;
+            OnPropertyChanged("AllDepartments");
+        }
+    }
+
+    // Все позиции
+    private List<Position> allPositions = DataWorker.GetAllPositions();
+
+    public List<Position> AllDepartments {
+        get => allDepartments;
+        set {
+            allDepartments = value;
+            OnPropertyChanged("AllDepartments");
+        }
+    }
+
 }
