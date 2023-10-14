4a54f989f21fafd933d17e84c0da8f58dd328746
Vik154 <Smorodin_1209@mail.ru>
2023-10-14 21:37:05 +0700

    Класс исключений бронирования номеров

---
+++ Projects_Examples/Проект_6/HotelReservation/Exceptions/ReservationConflictException.cs
@@ -0,0 +1,28 @@
+﻿using HotelReservation.Models;
+
+namespace HotelReservation.Exceptions;
+
+/// <summary> Класс исключений бронирования номеров </summary>
+public class ReservationConflictException : Exception {
+
+    /// <summary> Существующее бронирование </summary>
+    public Reservation ExistingReservation { get; }
+
+    /// <summary> Поступающее бронирование </summary>
+    public Reservation IncomingReservation { get; }
+
+    public ReservationConflictException(Reservation existingReservation, Reservation incomingReservation) {
+        ExistingReservation = existingReservation;
+        IncomingReservation = incomingReservation;
+    }
+
+    public ReservationConflictException(string? message, Reservation existingReservation, Reservation incomingReservation) : base(message) {
+        ExistingReservation = existingReservation;
+        IncomingReservation = incomingReservation;
+    }
+
+    public ReservationConflictException(string? message, Exception? innerException, Reservation existingReservation, Reservation incomingReservation) : base(message, innerException) {
+        ExistingReservation = existingReservation;
+        IncomingReservation = incomingReservation;
+    }
+}
