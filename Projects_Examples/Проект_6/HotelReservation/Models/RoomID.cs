29ec1649c249dea71d56d475eb473cf6600b487e
Vik154 <Smorodin_1209@mail.ru>
2023-10-14 22:01:54 +0700

    Идентификационный номер номера отеля

--- Projects_Examples/Проект_6/HotelReservation/Models/RoomID.cs
+++ Projects_Examples/Проект_6/HotelReservation/Models/RoomID.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelReservation.Models;
+﻿namespace HotelReservation.Models;
 
 /// <summary> Идентификационный номер номера отеля </summary>
 public class RoomID {
@@ -47,8 +41,25 @@ public class RoomID {
     /// <summary>
     /// Возвращает номер этажа и номер комнаты
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Номер этажа и номер комнаты</returns>
     public override string ToString() {
         return $"{FloorNumber}{RoomNumber}";
+    }
+
+    /// <summary>
+    /// Проверка на null, т.к. Equals возвращает false если оба null
+    /// </summary>
+    /// <param name="roomID1"></param>
+    /// <param name="roomID2"></param>
+    /// <returns></returns>
+    public static bool operator ==(RoomID roomID1, RoomID roomID2) {
+        if (roomID1 is null && roomID2 is null) {
+            return true;
+        }
+        return !(roomID1 is null) && roomID1.Equals(roomID2);
+    }
+
+    public static bool operator !=(RoomID roomID1, RoomID roomID2) {
+        return !(roomID1 == roomID2);
     }
 }
