4673853c1188b5bb9d3db3a144c844cf2c2f656a
Vik154 <Smorodin_1209@mail.ru>
2023-10-14 21:15:44 +0700

    Запись на бронирование

---
+++ Projects_Examples/Проект_6/HotelReservation/Models/Reservation.cs
@@ -0,0 +1,33 @@
+﻿namespace HotelReservation.Models;
+
+/// <summary> Запись на бронирование </summary>
+public class Reservation {
+
+    /// <summary> Имя пользователя зарезервировавшего номер </summary>
+    public string UserName { get; }
+
+    /// <summary> Идентификатор номера отеля </summary>
+    public RoomID RoomID { get; }
+
+    /// <summary> Время начала бронирования </summary>
+    public DateTime StartTime { get; }
+
+    /// <summary> Время окончания брони </summary>
+    public DateTime EndTime { get; }
+
+    /// <summary> Продолжительность времени бронирования </summary>
+    public TimeSpan Length => EndTime.Subtract(StartTime);
+
+    /// <summary>
+    /// Конструктор принимающий ID, Время начала и окончания срока бронирования номера
+    /// </summary>
+    /// <param name="roomID"></param>
+    /// <param name="startTime"></param>
+    /// <param name="endTime"></param>
+    public Reservation(RoomID roomID, string userName, DateTime startTime, DateTime endTime) {
+        RoomID = roomID;
+        UserName = userName;
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+}
