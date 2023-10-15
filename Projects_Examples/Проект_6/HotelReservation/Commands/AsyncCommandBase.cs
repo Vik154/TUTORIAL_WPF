e421aa8da45737b3ab10b853cf068900050a9c24
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 20:26:05 +0700

    // Базовый класс асинхронной команды

---
+++ Projects_Examples/Проект_6/HotelReservation/Commands/AsyncCommandBase.cs
@@ -0,0 +1,28 @@
+﻿namespace HotelReservation.Commands;
+
+// Базовый класс асинхронной команды
+public abstract class AsyncCommandBase : BaseCommand {
+
+    private bool _isExecuting;
+    private bool IsExecuting {
+        get => _isExecuting;
+        set { _isExecuting = value; OnCanExecutedChanged(); }
+    }
+
+    public override bool CanExecute(object? parameter) {
+        return !IsExecuting && base.CanExecute(parameter);
+    }
+
+    public override async void Execute(object? parameter) {
+        IsExecuting = true;
+
+        try {
+            await ExecuteAsync(parameter);
+        }
+        finally {
+            IsExecuting = false;
+        }
+    }
+
+    public abstract Task ExecuteAsync(object parameter);
+}
