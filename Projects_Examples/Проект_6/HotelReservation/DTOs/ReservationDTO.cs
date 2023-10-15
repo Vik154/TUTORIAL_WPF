e1a9a6080e29491577684a48a7e4248a87110222
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 19:21:10 +0700

    Класс - прослойка между логикой и базой данных

---
+++ Projects_Examples/Проект_6/HotelReservation/DTOs/ReservationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservation.DTOs;
+
+/// <summary> Класс - прослойка между логикой и базой данных </summary>
+public class ReservationDTO {
+    [Key]
+    public Guid Id { get; set; }
+    public int FloorNumber { get; set; }
+    public int RoomNumber { get; set; }
+    public string Username { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
