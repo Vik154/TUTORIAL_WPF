b898993e3757be073e6cb0a6ebce096f16c0ac8d
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 17:33:02 +0700

    Навигация между моделями - представлений

---
+++ Projects_Examples/Проект_6/HotelReservation/Stores/NavigationStore.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservation.ViewModels;
+
+namespace HotelReservation.Stores;
+
+/// <summary> Навигация между моделями - представлений </summary>
+public class NavigationStore {
+
+    private BaseViewModel? _currentViewModel;
+    public BaseViewModel? CurrentViewModel {
+        get => _currentViewModel;
+        set {
+            _currentViewModel?.Dispose();
+            _currentViewModel = value;
+            OnCurrentViewModelChanged();
+        }
+    }
+
+    public event Action? CurrentViewModelChanged;
+
+    private void OnCurrentViewModelChanged() {
+        CurrentViewModelChanged?.Invoke();
+    }
+}
