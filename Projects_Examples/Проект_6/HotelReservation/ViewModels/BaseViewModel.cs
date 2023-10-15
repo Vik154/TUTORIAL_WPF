d9611af923ecb0a48612b94a2c767fc039b73e92
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 13:02:02 +0700

    Реализация базовой view model

---
+++ Projects_Examples/Проект_6/HotelReservation/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace HotelReservation.ViewModels;
+
+/// <summary> Базовая модель - представления </summary>
+public class BaseViewModel : INotifyPropertyChanged {
+
+    /// <summary> Событие генерируемое при изменении свойств </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary> Метод, вызывающий событие при изменении свойств </summary>
+    /// <param name="propertyName"></param>
+    protected void OnPropertyChanged(string propertyName) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary> Освобождение захваченных ресурсов, реализуется в наследниках </summary>
+    public virtual void Dispose() { }
+}
