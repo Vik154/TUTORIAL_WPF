de8d9f9ef5112d2a4f88612d782aee4064317ade
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 19:41:14 +0700

    Интерфейс поставщика данных

---
+++ Projects_Examples/Проект_6/HotelReservation/Services/ReservationProviders/IReservationProvider.cs
@@ -0,0 +1,8 @@
+﻿using HotelReservation.Models;
+
+namespace HotelReservation.Services.ReservationProviders;
+
+/// <summary> Интерфейс поставщика данных </summary>
+public interface IReservationProvider {
+    Task<IEnumerable<Reservation>> GetAllReservations();
+}
