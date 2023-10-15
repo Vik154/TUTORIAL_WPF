e0f3ae31de859431bad00ad1053083cbbb18d1d0
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 13:40:01 +0700

    Модель - представление зарезервированных номеров

--- Projects_Examples/Проект_6/HotelReservation/ViewModels/ReservationViewModel.cs
+++ Projects_Examples/Проект_6/HotelReservation/ViewModels/ReservationViewModel.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HotelReservation.Models;
 
 namespace HotelReservation.ViewModels;
 
+/// <summary> Модель - представление зарезервированных номеров </summary>
 public class ReservationViewModel : BaseViewModel {
+    
+    /// <summary> Зарезервированные номера </summary>
+    private readonly Reservation _reservation;
 
+    /// <summary> Возвращает номер комнаты </summary>
+    public string? RoomID => _reservation.RoomID?.ToString();
+
+    /// <summary> Возвращает имя пользователя </summary>
+    public string Username => _reservation.UserName;
+
+    /// <summary> Возвращает время начала брони </summary>
+    public string StartDate => _reservation.StartTime.ToString("d");
+
+    /// <summary> Возвращает окончания начала брони </summary>
+    public string EndDate => _reservation.EndTime.ToString("d");
+
+    public ReservationViewModel(Reservation reservation) {
+        _reservation = reservation;
+    }
 }
