77fb5efa0d892090d7e3dddeba4e337d25518cbe
Vik154 <Smorodin_1209@mail.ru>
2023-10-15 14:41:01 +0700

    Базовый класс команды

---
+++ Projects_Examples/Проект_6/HotelReservation/Commands/BaseCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace HotelReservation.Commands;
+
+public abstract class BaseCommand : ICommand {
+    public event EventHandler? CanExecuteChanged;
+    public virtual bool CanExecute(object? parameter) => true;
+    public abstract void Execute(object? parameter);
+    protected void OnCanExecutedChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+}
