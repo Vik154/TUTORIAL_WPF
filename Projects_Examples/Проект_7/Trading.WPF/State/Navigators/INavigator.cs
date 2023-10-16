3e4ac8a2ad08fef2ea270f87220e8a1d58177054
Vik154 <Smorodin_1209@mail.ru>
2023-10-17 00:14:10 +0700

    Интерфейс навигации между представлениями

---
+++ Projects_Examples/Проект_7/Trading.WPF/State/Navigators/INavigator.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using Trading.WPF.ViewModels;
+
+namespace Trading.WPF.State.Navigators;
+
+/// <summary> Интерфейс навигации между представлениями </summary>
+public interface INavigator {
+
+    /// <summary> Указатель на базовый класс моделей - представления </summary>
+    BaseViewModel CurrentViewModel { get; set; }
+
+    /// <summary> Указатель на базовый интерфейс навигационной команды </summary>
+    ICommand UpdateCurrentViewModelCommand { get; }
+}
