cf091e6eb58d8e7794f84445d7b78c5b10c2241c
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 16:18:33 +0700

    Уборка мусора

--- Projects_Examples/Проект_6/HotelReservation/Views/MakeReservationView.xaml.cs
+++ Projects_Examples/Проект_6/HotelReservation/Views/MakeReservationView.xaml.cs
@@ -1,25 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace HotelReservation.Views {
-    /// <summary>
-    /// Логика взаимодействия для MakeReservationView.xaml
-    /// </summary>
-    public partial class MakeReservationView : UserControl {
-        public MakeReservationView() {
-            InitializeComponent();
-        }
-    }
+namespace HotelReservation.Views; 
+
+public partial class MakeReservationView : UserControl {
+    public MakeReservationView() => InitializeComponent();
 }
