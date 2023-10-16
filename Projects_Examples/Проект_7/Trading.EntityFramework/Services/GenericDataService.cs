9aa4b1ca41628a9211db3a6421bc1295d5291bec
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 23:27:39 +0700

    Реализация сервиса передачи данных

--- Projects_Examples/Проект_7/Trading.EntityFramework/Services/GenericDataService.cs
+++ Projects_Examples/Проект_7/Trading.EntityFramework/Services/GenericDataService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Trading.Domain.Models;
 using Trading.Domain.Services;
 
 namespace Trading.EntityFramework.Services;
@@ -7,7 +9,7 @@ namespace Trading.EntityFramework.Services;
 /// Универсальный сервис передачи данных (обработка всех операций от доменной модели) 
 /// чтобы не создавать сервис для каждой модели отдельно
 /// </summary>
-public class GenericDataService<T> : IDataService<T> where T : class {
+public class GenericDataService<T> : IDataService<T> where T : DomainObject {
 
     /// <summary>Контекст данных</summary>
     private readonly SimpleTraderDbContextFactory _contextFactory;
@@ -18,31 +20,50 @@ public class GenericDataService<T> : IDataService<T> where T : class {
 
     public async Task<T> Create(T entity) {
         using (SimpleTraderDbContext db = _contextFactory.CreateDbContext()) {
-            var createdEntity = await db.Set<T>().AddAsync(entity);
+            
+            EntityEntry<T> createdEntity = await db.Set<T>().AddAsync(entity);
             await db.SaveChangesAsync();
 
             return createdEntity.Entity;
         }
     }
 
-    public Task<bool> Delete(int id) {
+    public async Task<bool> Delete(int id) {
         using (SimpleTraderDbContext db = _contextFactory.CreateDbContext()) {
-            T findEntity = await db.Set<T>().FirstOrDefaultAsync((e) => e.)
+            
+            T? findEntity = await db.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+            
+            if (findEntity != null)
+                db.Set<T>().Remove(findEntity);
+            
             await db.SaveChangesAsync();
 
-            return createdEntity.Entity;
+            return true;
         }
     }
 
-    public Task<T> Get(int id) {
-        throw new NotImplementedException();
+    public async Task<T> Get(int id) {
+        using (SimpleTraderDbContext db = _contextFactory.CreateDbContext()) {
+            T findEntity = await db.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+            return findEntity;
+        }
     }
 
-    public Task<IEnumerable<T>> GetAll() {
-        throw new NotImplementedException();
+    public async Task<IEnumerable<T>> GetAll() {
+        using (SimpleTraderDbContext db = _contextFactory.CreateDbContext()) {
+            IEnumerable<T> entities = await db.Set<T>().ToListAsync();
+            return entities;
+        }
     }
 
-    public Task<T> Update(int id, T entity) {
-        throw new NotImplementedException();
+    public async Task<T> Update(int id, T entity) {
+        using (SimpleTraderDbContext db = _contextFactory.CreateDbContext()) {
+
+            entity.Id = id;
+            db.Set<T>().Update(entity);
+            await db.SaveChangesAsync();
+
+            return entity;
+        }
     }
 }
