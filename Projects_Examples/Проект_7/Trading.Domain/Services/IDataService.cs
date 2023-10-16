d94b46077a8d07b7b06dd15336dfd16283c6b8f8
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 20:06:06 +0700

    Добавление сервиса получения данных из БД

---
+++ Projects_Examples/Проект_7/Trading.Domain/Services/IDataService.cs
@@ -0,0 +1,20 @@
+﻿namespace Trading.Domain.Services;
+
+/// <summary> Интерфейс получения данных из БД </summary>
+public interface IDataService<T> {
+
+    /// <summary> Получить все данные </summary>
+    Task<IEnumerable<T>> GetAll();
+
+    /// <summary> Получить данные по идентификатору </summary>
+    Task<T> Get(int id);
+
+    /// <summary> Создать данные </summary>
+    Task<T> Create(T entity);
+
+    /// <summary> Обновить данные </summary>
+    Task<T> Update(int id, T entity);
+
+    /// <summary> Удалить данные </summary>
+    Task<bool> Delete(int id);
+}
