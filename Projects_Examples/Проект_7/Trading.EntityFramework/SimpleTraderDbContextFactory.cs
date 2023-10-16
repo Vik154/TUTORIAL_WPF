b7300d60107bdfb414d05dcc96ddcaf4fdf4762b
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 21:49:15 +0700

    Rename

--- Projects_Examples/Проект_7/Trading.EntityFramework/SimpleTraderDbContextFactory.cs
+++ Projects_Examples/Проект_7/Trading.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace Trading.EntityFramework;
 
 /// <summary> При запуске миграций EF запускает наследников IDesignTime... </summary>
-public class DesignTimeDbContextOptionsFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext> {
+public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext> {
     public SimpleTraderDbContext CreateDbContext(string[] args) {
         var optioms = new DbContextOptionsBuilder<SimpleTraderDbContext>();
         optioms.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TradingDB;Trusted_Connection=True;");
