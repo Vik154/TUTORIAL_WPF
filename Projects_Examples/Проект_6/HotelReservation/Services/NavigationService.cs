b99fb9c80f2ebefd0c4a5da8479dc95bf3aafff1
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 14:37:40 +0700

    Финальная логика сервиса навигации по представлениям

--- Projects_Examples/Проект_6/HotelReservation/Services/NavigationService.cs
+++ Projects_Examples/Проект_6/HotelReservation/Services/NavigationService.cs
@@ -5,11 +5,11 @@ namespace HotelReservation.Services;
 
 /// <summary> Сервис для переключения моделей - представления </summary>
 /// <typeparam name="TViewModel"></typeparam>
-public class NavigationService {
+public class NavigationService<TViewModel> where TViewModel : BaseViewModel {
     private readonly NavigationStore _navigationStore;
-    private readonly Func<BaseViewModel> _createViewModel;
+    private readonly Func<TViewModel> _createViewModel;
 
-    public NavigationService(NavigationStore navigationStore, Func<BaseViewModel> createViewModel) {
+    public NavigationService(NavigationStore navigationStore, Func<TViewModel> createViewModel) {
         _navigationStore = navigationStore;
         _createViewModel = createViewModel;
     }
