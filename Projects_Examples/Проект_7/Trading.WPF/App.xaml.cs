91390fac06b89dcbccfa0beef62491db3e94cd97
Vik154 <Smorodin_1209@mail.ru>
2023-10-16 23:52:59 +0700

    Добавление инфраструктуры MVVM

---
+++ Projects_Examples/Проект_7/Trading.WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Trading.WPF; 
+
+
+public partial class App : Application {
+
+    protected override void OnStartup(StartupEventArgs e) {
+
+        Window window = new Window();
+        window.Show();
+
+        base.OnStartup(e);
+    }
+}
