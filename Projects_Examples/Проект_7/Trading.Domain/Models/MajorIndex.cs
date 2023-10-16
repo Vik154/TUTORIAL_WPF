a02c4f151e3ab24f1d29c3d2c2ef27b6a7249c21
Vik154 <Smorodin_1209@mail.ru>
2023-10-17 01:30:27 +0700

    Добаление интерфейса биржевых индексов для работы с api

---
+++ Projects_Examples/Проект_7/Trading.Domain/Models/MajorIndex.cs
@@ -0,0 +1,12 @@
+﻿namespace Trading.Domain.Models;
+
+/// <summary> Перечисление основных индексов </summary>
+public enum MajorIndexType { DowJones, Nasdaq, SP500 }
+
+/// <summary> Класс представляющий данные об индексе </summary>
+public class MajorIndex {
+    public string IndexName { get; set; } = "";
+    public double Price { get; set; }
+    public double Changes { get; set; }
+    public MajorIndexType Type { get; set; }
+}
---
+++ Projects_Examples/Проект_7/Trading.Domain/Services/IMajorIndexService.cs
@@ -0,0 +1,8 @@
+﻿using Trading.Domain.Models;
+
+namespace Trading.Domain.Services;
+
+/// <summary>Интерфейс представляющий индексы</summary>
+public interface IMajorIndexService {
+    Task<MajorIndex> GetMajorIndex(MajorIndexType indexType);
+}
