731d985bda7ae90b00d26b460f095e7ddef35d8e
Vik154 <Smorodin_1209@mail.ru>
2023-10-22 02:55:26 +0700

    Оповещение об ошибках

---
+++ Projects_Examples/Проект_7/Trading.WPF/ViewModels/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Trading.WPF.ViewModels;
+
+public class MessageViewModel : BaseViewModel {
+    private string _message;
+    public string Message {
+        get {
+            return _message;
+        }
+        set {
+            _message = value;
+            OnPropertyChanged(nameof(Message));
+            OnPropertyChanged(nameof(HasMessage));
+        }
+    }
+
+    public bool HasMessage => !string.IsNullOrEmpty(Message);
+}
