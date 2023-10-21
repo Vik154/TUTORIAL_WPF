7bf5857e30315726099647eed7174ea7bcfcc3b2
Vik154 <Smorodin_1209@mail.ru>
2023-10-22 01:44:45 +0700

    Хранитель сделок

---
+++ Projects_Examples/Проект_7/Trading.WPF/State/Assets/AssetStore.cs
@@ -0,0 +1,23 @@
+﻿using Trading.Domain.Models;
+using Trading.WPF.State.Accounts;
+
+namespace Trading.WPF.State.Assets;
+
+public class AssetStore {
+
+    private readonly IAccountStore _accountStore;
+
+    public double AccountBalance => _accountStore.CurrentAccount?.Balance ?? 0;
+    public IEnumerable<AssetTransaction> AssetTransactions => _accountStore.CurrentAccount?.AssetTransactions ?? new List<AssetTransaction>();
+
+    public event Action StateChanged;
+
+    public AssetStore(IAccountStore accountStore) {
+        _accountStore = accountStore;
+        _accountStore.StateChanged += OnStateChanged;
+    }
+
+    private void OnStateChanged() {
+        StateChanged?.Invoke();
+    }
+}
