169714e5083aef916f0d03cf739a90823597d221
Vik154 <Smorodin_1209@mail.ru>
2023-10-22 03:43:48 +0700

    Добавление сервиса продажи акций

---
+++ Projects_Examples/Проект_7/Trading.Domain/Services/TransactionServices/ISellStockService.cs
@@ -0,0 +1,6 @@
+﻿using Trading.Domain.Models;
+
+namespace Trading.Domain.Services.TransactionServices;
+public interface ISellStockService {
+    Task<Account> SellStock(Account seller, string symbol, int shares);
+}
--- Projects_Examples/Проект_7/Trading.WPF/ViewModels/PortfolioViewModel.cs
+++ Projects_Examples/Проект_7/Trading.WPF/ViewModels/PortfolioViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trading.WPF.State.Assets;
 
 namespace Trading.WPF.ViewModels;
 
-public class PortfolioViewModel : BaseViewModel
-{
+public class PortfolioViewModel : BaseViewModel {
+    public AssetListingViewModel AssetListingViewModel { get; }
+
+    public PortfolioViewModel(AssetStore assetStore) {
+        AssetListingViewModel = new AssetListingViewModel(assetStore);
+    }
+
+    public override void Dispose() {
+        AssetListingViewModel.Dispose();
+
+        base.Dispose();
+    }
 }
