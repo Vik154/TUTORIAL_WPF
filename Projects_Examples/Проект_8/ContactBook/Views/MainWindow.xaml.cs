3a191db79228c137cf3fac0df588629b24948fdf
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 06:53:55 +0700

    Добавление пакетов MaterialTheme & Icon

--- Projects_Examples/Проект_8/ContactBook/Views/MainWindow.xaml.cs
+++ Projects_Examples/Проект_8/ContactBook/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,12 +10,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ContactBook {
+namespace ContactBook.Views {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
