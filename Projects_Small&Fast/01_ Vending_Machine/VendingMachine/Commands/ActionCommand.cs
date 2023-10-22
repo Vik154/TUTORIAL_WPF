e421e290d077035e96dcb23d5768b5116cd1368a
Vik154 <Smorodin_1209@mail.ru>
2023-10-22 10:35:46 +0700

    Первоначальная разметка

---
+++ Projects_Small&Fast/01_ Vending_Machine/VendingMachine/Commands/ActionCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace VendingMachine.Commands;
+
+public class ActionCommand : BaseCommand {
+    private readonly Action<object> _Execute;
+    private readonly Func<object, bool>? _CanExecute;
+
+    public ActionCommand(Action Execute, Func<bool>? CanExecute = null)
+        : this(p => Execute(), CanExecute is null ? (Func<object, bool>?)null : p => CanExecute()) { }
+
+    public ActionCommand(Action<object> Execute, Func<object, bool>? CanExecute = null) {
+        _Execute = Execute;
+        _CanExecute = CanExecute;
+    }
+
+    protected override bool CanExecute(object p) => _CanExecute?.Invoke(p) ?? true;
+
+    protected override void Execute(object p) => _Execute(p);
+}
