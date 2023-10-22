e76e62426327f0cddee7d735d09408d0d6fb473a
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 06:56:21 +0700

    Базовая реализация уведомлений об изменениях свойств

---
+++ Projects_Examples/Проект_8/ContactBook/Utility/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ContactBook.Utility;
+
+/// <summary> Базовая реализация уведомлений об изменениях свойств </summary>
+public class ObservableObject : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool OnPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "") {
+        if (EqualityComparer<T>.Default.Equals(backingField, value))
+            return false;
+
+        backingField = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
