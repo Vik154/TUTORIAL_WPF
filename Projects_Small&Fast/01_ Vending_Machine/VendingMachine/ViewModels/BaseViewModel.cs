450a9cea5376216fa24bf20fdbdbb6a1a94afe39
Vik154 <Smorodin_1209@mail.ru>
2023-10-22 10:03:58 +0700

    Базовая ViewModel

--- Projects_Small&Fast/01_ Vending_Machine/VendingMachine/ViewModels/BaseViewModel.cs
+++ Projects_Small&Fast/01_ Vending_Machine/VendingMachine/ViewModels/BaseViewModel.cs
@@ -9,11 +9,4 @@ public abstract class BaseViewModel : INotifyPropertyChanged {
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null) =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
-
-    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null) {
-        if (Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(PropertyName);
-        return true;
-    }
 }
