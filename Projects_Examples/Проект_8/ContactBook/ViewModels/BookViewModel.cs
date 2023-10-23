8fa1aeec569c1908a96b46a88cd34812c3a5685b
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 09:59:32 +0700

    Добавление команд загрузки контактов

--- Projects_Examples/Проект_8/ContactBook/ViewModels/BookViewModel.cs
+++ Projects_Examples/Проект_8/ContactBook/ViewModels/BookViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using ContactBook.Utility;
+using System.Windows.Input;
 
 namespace ContactBook.ViewModels;
 
 public class BookViewModel : ObservableObject {
 
+    #region СВОЙСТВА
     /// <summary> Заголовок главного окна </summary>
     private string _title = "Книга";
 
@@ -12,4 +14,46 @@ public class BookViewModel : ObservableObject {
         get => _title;
         set => OnPropertyChanged(ref _title, value);
     }
+
+    /// <summary> Модель - представление списка контактов </summary>
+    private ContactsViewModel? _contactsViewModel;
+
+    /// <summary> Модель - представление списка контактов </summary>
+    private ContactsViewModel? ContactsViewModel {
+        get => _contactsViewModel;
+        set => OnPropertyChanged(ref _contactsViewModel, value);
+    }
+    #endregion
+
+    #region КОМАНДЫ
+    /// <summary> Загрузка списка контактов </summary>
+    public ICommand LoadContactsCommand { get; private set; }
+
+    /// <summary> Загрузка списка избранных контактов </summary>
+    public ICommand LoadFavoritesContactsCommand { get; private set; }
+
+    #endregion
+
+    #region КОНСТРУКТОРЫ
+    public BookViewModel() {
+        ContactsViewModel = new ContactsViewModel();
+
+        LoadContactsCommand = new RelayCommand(LoadContacts);
+        LoadFavoritesContactsCommand = new RelayCommand(LoadFavorites);
+        
+    }
+    #endregion
+
+    #region МЕТОДЫ ДЛЯ КОМАНД
+
+    /// <summary> Загрузка списка контактов </summary>
+    private void LoadContacts() {
+
+    }
+
+    /// <summary> Загрузка списка избранных контактов </summary>
+    public void LoadFavorites() {
+
+    }
+    #endregion
 }
---
+++ Projects_Examples/Проект_8/ContactBook/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ContactBook.Models;
+using ContactBook.Utility;
+using System.Collections.ObjectModel;
+
+namespace ContactBook.ViewModels;
+
+/// <summary> Модель - представление хранящая все существующие контакты в записной книге </summary>
+public class ContactsViewModel : ObservableObject {
+
+    /// <summary> Выбранный контакт из списка контактов </summary>
+    private Contact? _selectedContact;
+
+    /// <summary> Выбранный контакт из списка контактов </summary>
+    public Contact? SelectedContact {
+        get => _selectedContact;
+        set => OnPropertyChanged(ref _selectedContact, value);
+    }
+
+    /// <summary> Список контактов </summary>
+    public ObservableCollection<Contact> Contacts { get; set; }
+
+    public ContactsViewModel() { }
+
+    /// <summary> Загрузка списка контактов </summary>
+    public void LoadContacts(IEnumerable<Contact> contacts) {
+        Contacts = new ObservableCollection<Contact>(contacts);
+        OnPropertyChanged("Contacts");
+    }
+}
