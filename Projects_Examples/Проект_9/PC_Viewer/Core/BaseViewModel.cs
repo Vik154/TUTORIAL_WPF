e6ca00b17790f94b3dbf64daea1e9f083924dc92
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 23:34:15 +0700

    Добавление базовой модели - представления

---
+++ Projects_Examples/Проект_9/PC_Viewer/Core/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PC_Viewer.Core;
+
+public abstract class BaseViewModel : INotifyPropertyChanged {
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool OnPropertyChanged<T>(ref T field, T value, [CallerMemberName] string propertyName = "") {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
