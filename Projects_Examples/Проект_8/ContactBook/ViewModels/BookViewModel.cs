9ae935851384339a18500b74238a8725d7954fab
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 08:53:35 +0700

    Добавления шаблона данных в ресурсы, для взаимосвязи View с ViewModel

---
+++ Projects_Examples/Проект_8/ContactBook/ViewModels/BookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ContactBook.Utility;
+
+namespace ContactBook.ViewModels;
+
+public class BookViewModel : ObservableObject {
+
+    /// <summary> Заголовок главного окна </summary>
+    private string _title = "Книга";
+
+    /// <summary> Заголовок главного окна </summary>
+    public string Title {
+        get => _title;
+        set => OnPropertyChanged(ref _title, value);
+    }
+}
--- Projects_Examples/Проект_8/ContactBook/ViewModels/MainViewModel.cs
+++ Projects_Examples/Проект_8/ContactBook/ViewModels/MainViewModel.cs
@@ -4,10 +4,44 @@ namespace ContactBook.ViewModels;
 
 public class MainViewModel : ObservableObject {
 
+    #region ПОЛЯ
+
+    #endregion
+
+    #region СВОЙСТВА
+
+    /// <summary> Заголовок главного окна </summary>
     private string _titleWindow = "Книга контактов";
+
+    /// <summary> Заголовок главного окна </summary>
     public string TitleWindow {
         get => _titleWindow;
         set => OnPropertyChanged(ref  _titleWindow, value);
+    }
+
+    /// <summary> Текущее представление </summary>
+    private object? _currentView;
+
+    /// <summary> Текущее представление </summary>
+    public object? CurrentView {
+        get => _currentView;
+        set => OnPropertyChanged(ref _currentView, value);
+    }
+
+    /// <summary> Модель представление - записной книги </summary>
+    private BookViewModel? _bookViewModel;
+
+    /// <summary> Модель представление - записной книги </summary>
+    public BookViewModel? BookViewModel {
+        get => _bookViewModel;
+        set => OnPropertyChanged(ref _bookViewModel, value);
+    }
+
+    #endregion
+
+    public MainViewModel() {
+        BookViewModel = new BookViewModel();
+        CurrentView = BookViewModel;
     }
 
 }
