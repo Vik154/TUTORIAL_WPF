b2497cddac864b627d9230b511d6f08bb7f2c377
Vik154 <Smorodin_1209@mail.ru>
2023-10-23 23:24:35 +0700

    Добавление базовой команды

---
+++ Projects_Examples/Проект_9/PC_Viewer/Core/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace PC_Viewer.Core;
+
+public class RelayCommand : ICommand {
+    private readonly Action<object> _Execute;
+    private readonly Func<object, bool>? _CanExecute;
+
+    public event EventHandler? CanExecuteChanged {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public RelayCommand(Action Execute, Func<bool>? CanExecute = null)
+        : this(p => Execute(), CanExecute is null ? (Func<object, bool>?)null : p => CanExecute()) { }
+
+    public RelayCommand(Action<object> Execute, Func<object, bool>? CanExecute = null) {
+        _Execute = Execute;
+        _CanExecute = CanExecute;
+    }
+
+    public bool CanExecute(object? p) => _CanExecute?.Invoke(p) ?? true;
+
+    public void Execute(object? p) => _Execute(p);
+}
